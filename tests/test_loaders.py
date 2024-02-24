94438715f693270d65edc50e4d1598c64304f989
DanielMuringe <danielmuringe@gmail.com>
2024-02-24 15:33:38 +0300

    Create and passed tests

---
+++ tests/test_loaders.py
@@ -0,0 +1,72 @@
+"""Test main loading functions"""
+
+# Second-party imports
+from typed_env import load, load_file, load_space
+
+
+def test_load_space(final_data, included_vars, excluded_vars):
+    """Test the load_space function"""
+
+    # Test return value
+    vars_ = load_space(included_vars)
+    assert vars_ == final_data
+
+    for excluded_var in excluded_vars:
+        assert excluded_var not in vars_
+
+    # Test global variables
+    load_space(included_vars, globals())
+    for var, val in final_data.items():
+        assert globals()[var] == val
+
+    for excluded_var in excluded_vars:
+        assert excluded_var not in vars_
+
+
+def test_load_file(final_data, data_dir, file_formats):
+    """Test the load_file function"""
+
+    for file_format in file_formats:
+        file_path = data_dir / f"env.{file_format}"
+
+        # Test return value
+        vars_ = load_file(file_path, file_format)
+        assert vars_ == final_data
+
+        # Test global variables
+        load_file(file_path, file_format, globals())
+        for var, val in final_data.items():
+            assert globals()[var] == val
+
+
+def test_load(final_data, file_formats, data_dir, included_vars, excluded_vars):
+    """Test the load function"""
+
+    # TEST FILE VARS
+    for file_format in file_formats:
+        file_path = data_dir / f"env.{file_format}"
+
+        # Test return value
+        vars_ = load(file_path, file_format)
+        assert vars_ == final_data
+
+        # Test global variables
+        load(file_path, file_format, globals())
+        for var, val in final_data.items():
+            assert globals()[var] == val
+
+    # TEST SPACE VARS
+    # Test return value
+    vars_ = load(included_vars)
+    assert vars_ == final_data
+
+    for excluded_var in excluded_vars:
+        assert excluded_var not in vars_
+
+    # Test global variables
+    load(included_vars, globals())
+    for var, val in final_data.items():
+        assert globals()[var] == val
+
+    for excluded_var in excluded_vars:
+        assert excluded_var not in vars_
--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,54 +1,24 @@
 """Test reader classes"""
 
-# PIP installed imports
-
 # Second-party imports
 from typed_env.parsers import EnvironParser, FileParser, environ
-from . import DATA_DIR
 
 
-DATA_TESTS = {
-    "INT_VAR": 1,
-    "FLOAT_VAR": 1.0,
-    "STRING_VAR": "hello",
-    "BOOL_VAR": True,
-    "LIST_INT_VAR": [1, 2, 3],
-    "LIST_FLOAT_VAR": [1.0, 2.0, 3.0],
-    "LIST_STRING_VAR": ["hello", "world"],
-    "LIST_BOOL_VAR": [True, False],
-    "DICT_VAR": {"name": "John", "age": 25},
-}
+def test_file_reader(final_data, data_dir, file_formats, excluded_vars):
+    """Test the file reader"""
+    for format_ in file_formats:
+        vars_ = FileParser(data_dir / f"env.{format_}", format_).vars
+        assert vars_ == final_data
+
+    for excluded_var in excluded_vars:
+        assert excluded_var not in vars_
 
 
-def test_readers_correlation():
-    """Ensure all file types return the same result"""
+def test_environ_reader(final_data, included_vars, excluded_vars):
+    """Test the environ reader"""
 
-    # Add variables to the environment exclude
-    environ["EXCLUDE"] = "['INT_VAR']"
+    vars_ = EnvironParser(included_vars).vars
+    assert vars_ == final_data
 
-    # TEST FILE READERS
-    formats = ["json", "toml", "yaml", "env"]
-
-    prev_test_data = None
-
-    for format_ in formats:
-        cur_test_data = FileParser(DATA_DIR / f"env.{format_}", format_).vars
-
-        if prev_test_data is None:
-            prev_test_data = cur_test_data
-
-        assert cur_test_data == prev_test_data
-
-    # TEST ENVIRON READER
-    # Load test variables in to the environment namespace
-
-    for var, val in prev_test_data.items():
-        environ[var] = str(val)
-
-    # Assert that the environ reader returns the same variables
-    assert EnvironParser(prev_test_data.keys()).vars == prev_test_data
-
-    # Assert that the excluded variable is not in the environ reader
-    assert "INT_VAR" not in prev_test_data
-
-    print(prev_test_data)
+    for excluded_var in excluded_vars:
+        assert excluded_var not in vars_
